9ae2a227a1baea4647600c6c6f8822f2ca415e7a
Matthias Gerhard <matthias.gerhard00@gmail.com>
2023-07-24 18:12:38 +0200

    qr code generator

---
+++ Project #006 - QR Code Generator/index.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+    $('#qrForm').submit(async (event) => {
+        event.preventDefault();
+        await generateQRCode($('#qrText').val());
+    });
+});
+
+async function generateQRCode (text) {
+    if (!text) return;
+    $('#image-container').empty();
+    $('#image-container').css('height', '0').removeClass('fadeIn');
+    $('.spinner').removeClass('d-none');
+    const url = 'https://api.api-ninjas.com/v1/qrcode?format=png&data=' + text;
+    const params = {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': 'rJ8QTkX/vlOlpRudaks+EQ==ndGROmLmQ3wl1V7f',
+            'Accept': 'image/png'
+        }
+    }
+
+    await fetch(url, params)
+        .then((response) => response.blob())
+        .then((blob) => {
+            $('.spinner').addClass('d-none');
+            let src = URL.createObjectURL(blob);
+            $('#image-container').append('<a title=\"Click to download!\" href=\"' 
+                + src + 
+                "\" download><img src=\"" 
+                +  src + 
+                "\" /></a>");
+            $('#image-container').css('height', '260px').addClass('fadeIn');
+        });
+}
