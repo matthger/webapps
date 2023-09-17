be3909940ef470b45a8edbd2b8fa8897217c0ebb
Matthias Gerhard <matthias.gerhard00@gmail.com>
2023-09-17 13:38:28 +0200

    ytmp3

---
+++ Project #007 - YTMP3 Converter/index.js
@@ -0,0 +1,41 @@
+$(document).ready(() => {
+    $('#ytForm').submit(async (event) => {
+        event.preventDefault();
+        await mp3Conversion($('#ytId').val());
+    });
+});
+
+async function mp3Conversion(id) {
+    if (!id) return;
+    $('#failAlert').addClass('d-none');
+    $('#resultContainer').css('height', '0').removeClass('fadeIn');
+    $('#videoTitle').text("");
+    $('#downloadLink').attr('href', '');
+    $('.spinner').removeClass('d-none');
+    const url = 'https://youtube-mp36.p.rapidapi.com/dl?id=' + id;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '2a6530744amsh1787e292e364c89p1a360fjsn9303106ee054',
+            'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+        }
+    };
+
+    let response = await fetch(url, options).then((response) => response.json());
+
+    if (response.status == "processing") {
+        setTimeout(async function() {
+            response = await fetch(url, option).then((response) => response.json());
+        }, 1000);
+    }
+
+    $('.spinner').addClass('d-none');
+
+    if (response.status == 'fail') {
+        $('#failAlert').removeClass('d-none');
+    } else {
+        $('#videoTitle').text(response.title);
+        $('#downloadLink').attr('href', response.link);
+        $('#resultContainer').css('height', '180px').addClass('fadeIn');
+    }
+}
